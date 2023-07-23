f42c452d1b8d8ca14d357e05d983f66a96a0b641
Hanzo127 <127133952+Hanzo127@users.noreply.github.com>
2023-07-23 07:36:35 +0700

    Update ppbot.js

--- js/ppbot.js
+++ js/ppbot.js
@@ -1,6 +1,11 @@
 const chalk = require('chalk')
 const fs = require('fs')
-global.ppbot = `https://telegra.ph/file/537877b0c11105d752bcd.jpg`// pakai url gmbr
+global.ppbot = const vi = ['https://telegra.ph/file/067b2cb3312837533239c.mp4',
+'https://telegra.ph/file/e38881701692c74484a17.mp4',
+'https://telegra.ph/file/de776d34ef058b7d2ec12.mp4', 'https://telegra.ph/file/bc82653506c301b40679c.mp4',     'https://telegra.ph/file/7f10b3624991bbcee9ded.mp4', 'https://telegra.ph/file/51aa9701839dcc29066e9.mp4',     'https://telegra.ph/file/4f26132ac0296a34a45a8.mp4']
+
+var vid = vi[Math.floor(Math.random() * (vi.length))]
+// pakai url gmbr
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 	fs.unwatchFile(file)
