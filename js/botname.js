dd9acc42d9d228c4df4241c6bc704af7af471717
Hanzo127 <127133952+Hanzo127@users.noreply.github.com>
2023-07-23 11:46:42 +0700

    Update botname.js

--- js/botname.js
+++ js/botname.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk')
 const fs = require('fs')
-global.botname = `丂ﾑズひ尺ﾑ`//nickhelp
+global.botname = `Nice to meet you|viper°`//nickhelp
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 	fs.unwatchFile(file)
